bd8ae30278ac519cbbd4f6835f0b42ba5fe33f9d
cnelias <cnelias@LAPTOP-F809CEH1.local>
2020-12-09 16:09:52 +0100

    update collisionMatrix.jl

--- src/CollisionMatrix.jl
+++ src/CollisionMatrix.jl
@@ -12,6 +12,27 @@ function S_matrix(ts, window)
     return S
 end
 
+"""
+Returns all possible masks of length d among the possible w positions.
+positive masks: the indexes represent the columns that are KEPT.
+"""
+function get_masks(w, d)
+    if d >= w
+        error("d must be smaller than w")
+    end
+    return collect(combinations(collect(1:w), w - d))
+end
+
+"""
+Returns a masked version of the S matrix.
+"""
+function apply_mask(mask, S)
+    masked_S = Array{typeof(S[1, 1]),2}(undef, size(S)[1], length(mask))
+    for (index, value) in enumerate(mask)
+        masked_S[:, index] = S[:, value]
+    end
+    return masked_S
+end
 
 test = ["a","b","c","d","a","b","c","a","b","c","a","b","c","a","b","c"]
 s = S_matrix(test, 4)
