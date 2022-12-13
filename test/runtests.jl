4f619f1e38643def3b3852d235604fbdee2858b7
Corentin Nélias <corentin.nelias@gmail.com>
2022-12-13 17:48:37 +0100

    Add tests for multiple inputs

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using DelimitedFiles
 using MotifRecognition
 
 cd(@__DIR__)
+#input containing a single timeseries
 data = readdlm("confirmation")
 pitch = mod.(data, 12)
 intervals = pitch[2:end] .- pitch[1:end-1]
@@ -11,3 +12,8 @@ consensus_shape = m[1].shape
 similar_motifs = find_motifs(intervals, consensus_shape, 1)
 @test m[1].shape == [-1.0, -2.0, 10.0, -10.0, 2.0, 3.0, 5.0]
 @test similar_motifs.instances[1] ==  [-1.0, -2.0, 10.0, -10.0, 2.0, 3.0, 5.0]
+
+#input including several timeseries
+data = [intervals[1:540], intervals[540:1080], intervals[1080:1620]]
+m = detect_motifs(intervals, 7, 1; iters = 700, tolerance = 0.7)
+
